0f3c160169ce217987dc429fa99de8c98b2061fc
Bohdan Ses <91324589+boderses@users.noreply.github.com>
2024-03-11 20:17:01 +0100

    test

--- myApp.js
+++ myApp.js
@@ -1,39 +1,7 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+app.get("/", function(req, res) {
+  res.send("Hello Express");
+});
