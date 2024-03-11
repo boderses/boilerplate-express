332fb524aedca3f7b576e6444e6b6d87b6c2c356
Bohdan Ses <91324589+boderses@users.noreply.github.com>
2024-03-11 20:35:10 +0100

    test

--- myApp.js
+++ myApp.js
@@ -1,8 +1,13 @@
 let express = require("express");
 let app = express();
 
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
+
+absolutePath = __dirname + '/views/index.html';
 app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
+    res.sendFile(absolutePath);
+} )
 
 module.exports = app;
